97734bbba86ca2bbdf95b7f21990091538db1991
thomas914 <thomasli914@gmail.com>
2021-11-16 18:22:03 +0800

    Updates

--- Underperforming-Students-vs-Teachers/mainwindow.cpp
+++ Underperforming-Students-vs-Teachers/mainwindow.cpp
@@ -1,15 +1,25 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "gamewindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowTitle("Underperforming Students VS Teachers");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_btn_start_clicked()
+{
+    this->hide();
+    gameWindow = new GameWindow(this);
+    gameWindow->show();
 }
 
